043832e17b1646fce7cb45297a256940e09c283e
Marcos Araujo Sobrinho <marcos.sobrinho@truckpad.com.br>
2017-08-10 17:56:18 -0300

    python package

---
+++ src/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='eclipse-che-provisioner',
+    version='0.0.1',
+    description='Eclipse Che Provisioner, for using with bit.ly oauth2_proxy, nginx and docker',
+    packages=find_packages(),
+    license='MIT',
+    author='Marcos Araujo Sobrinho',
+    author_email='marcos.sobrinho@truckpad.com.br',
+    url='https://github.com/truckpad/eclipse-che-multiuser/',
+    download_url='https://github.com/truckpad/eclipse-che-multiuser/',
+    keywords=['docker', 'eclipse', 'eclipse-che', 'ide'],
+    # long_description=open('README').read(),
+    scripts=['eclipse-che/provisioner.py'],
+    install_requires=open('requirements.txt').read().strip('\n').split('\n')
+)
